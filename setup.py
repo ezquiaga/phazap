9835e89ee4ac4ce18e012cdb657065c2a60092e3
Rico Lo <ka-lok.lo@ligo.org>
2023-10-22 12:46:13 -0700

    Restructure package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+verstr = "0.0.1"
+
+setuptools.setup(
+    name="phazap",
+    version=verstr,
+    author="Jose María Ezquiaga",
+    author_email="jose.ezquiaga@nbi.ku.dk",
+    description="Gravitational wave phase reconstruction for low-latency identification of strongly lensed signals",
+    packages=[
+        "phazap",
+    ],
+    install_requires=[
+        "configargparse",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    python_requires='>=3.7',
+)
